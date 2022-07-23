f002ee77571e171a1a2c3722865061e8c5cc82d8
Morkebla <109845755+Morkebla@users.noreply.github.com>
2022-07-23 21:08:43 +0300

    Added locations, and connected them

---
+++ Location.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrintian
+{
+    public class Location
+    {
+        public string Name { get; set; }
+        public List<Location> ConnectedLocations => _connectedLocations;
+        private List<Location> _connectedLocations = new List<Location>();
+
+       public Location()
+        {
+            Name = String.Empty;
+        }
+       public Location(string name)
+        {
+            Name=name;
+        }
+        public void ConnectSingle(params Location[] otherLocations)
+        {
+            foreach (Location otherLocation in otherLocations)
+            {
+                ConnectedLocations.Add(otherLocation);
+            }
+
+        }
+        public void ConnectBi(params Location[] otherLocations)
+        {
+            foreach (Location otherLocation in otherLocations)
+            {
+                this.ConnectedLocations.Add(otherLocation);
+                otherLocation.ConnectedLocations.Add(this);
+            }
+        }          
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Labyrintian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Location forest = new Location("Forest");
+            Location desert = new Location("Desert"); // to do random location transport
+            Location hills = new Location("Hills");
+            Location mountains = new Location("Mountains");
+            Location swamp = new Location("Swamp");
+            Location river = new Location("River");
+            Location town = new Location("Town");
+            Location cave = new Location("Cave");
+            Location lake = new Location("Lake");
+            Location waterfall = new Location("Waterfall");
+            Location plain = new Location("Plain");
+            Location snowfield = new Location("Snow Field");
+            Location field = new Location("Field");
+            Location farmland = new Location("Farmland");
+            Location ravine = new Location("Ravine");
+            Location castle = new Location("Castle");
+            forest.ConnectBi(hills,town,plain);
+            forest.ConnectSingle(river,desert);
+            river.ConnectSingle(waterfall, lake);
+            river.ConnectBi(town, farmland, plain);
+            hills.ConnectSingle(cave, ravine);
+            hills.ConnectBi(mountains, swamp);
+            mountains.ConnectSingle(cave,snowfield,waterfall,ravine);
+            swamp.ConnectSingle(cave, ravine);
+            swamp.ConnectBi(field);
+            town.ConnectSingle(mountains,town);
+            town.ConnectBi(farmland,castle);
+            desert.ConnectSingle(desert, desert, desert);
+        }
+        static void PrintLocation(Location location)
+        {
+            Console.WriteLine(location.Name);
+            foreach (Location connectedlocation in location.ConnectedLocations)
+                Console.WriteLine(" > " + connectedlocation.Name);
+        }
+    }
+}
